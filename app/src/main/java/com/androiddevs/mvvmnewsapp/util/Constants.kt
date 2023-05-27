d83f6d857c46da6fd2a2985ba168542fa1208272
Duy <ducduy@vtvcab.vn>
2023-05-27 10:43:51 +0700

    update

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "7e10404273644158a731bbc7f69c821d"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
