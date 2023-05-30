7b5c0f267d399f3db4f9449e501164dd9f8ee6d6
Duy <ducduy@vtvcab.vn>
2023-05-30 11:17:22 +0700

    update

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,7 @@ class Constants {
     companion object {
         const val API_KEY = "7e10404273644158a731bbc7f69c821d"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
